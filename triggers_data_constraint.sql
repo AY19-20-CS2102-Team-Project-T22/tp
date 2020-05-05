c4b537820feb91a95cc7e3a5aa33af0ab8be6baa
HAOYUN49 <e01261960@u.nus.edu>
2020-05-06 01:22:41 +0800

    Add data constraint trigger file

---
+++ triggers_data_constraint.sql
@@ -0,0 +1,79 @@
+/*ensure one customer can only choose food in one retaurant*/
+CREATE OR REPLACE FUNCTION check_restaurant () RETURNS TRIGGER AS $$
+DECLARE
+	restaurant		INTEGER;
+BEGIN
+	SELECT C.restaurantId INTO restaurant
+		FROM Carts C
+		WHERE NEW.cartId = C.cartId AND NEW.restaurantId <> C.restaurantId;
+	IF restaurant IS NOT NULL THEN
+		RAISE exception 'Food are from different restaurants';
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS check_restaurant_trigger ON Carts CASCADE;
+CREATE TRIGGER check_restaurant_trigger
+	BEFORE INSERT
+	ON Carts
+	FOR EACH ROW
+	EXECUTE FUNCTION check_restaurant();
+
+/*ensure every slot does not exceed 4 hours*/
+CREATE OR REPLACE FUNCTION check_work_slot() RETURNS TRIGGER AS $$
+BEGIN
+	IF NEW.endTime > NEW.startTime THEN
+		RAISE exception 'Working slot on % from %:00 to %:00 exceeds 4 hours', NEW.weekday, NEW.startTime, NEW.endTime;
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS check_work_slot_trigger ON WWS_Schedules CASCADE;
+CREATE TRIGGER check_work_slot_trigger
+	BEFORE UPDATE OF startTime, endTime OR INSERT
+	ON WWS_Schedules
+	FOR EACH ROW
+	EXECUTE FUNCTION check_work_slot();
+
+/*ensure total working hour >= 10 and <= 48*/
+CREATE OR REPLACE FUNCTION check_total_work_hour () RETURNS TRIGGER AS $$
+DECLARE
+	total_work_hour		INTEGER;
+BEGIN
+	SELECT sum(endTime - startTime) INTO total_work_hour
+	FROM WWS_Schedules W
+	WHERE NEW.workId = W.workId;
+
+	IF total_work_hour < 10 THEN
+		RAISE exception 'Total working hour within one week is less than 10 hours';
+	ELSIF total_work_hour > 48 THEN
+		RAISE exception 'Total working hour within one week is larger than 48 hours';
+	ELSE
+		RETURN NULL;
+	END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS check_total_work_hour_trigger ON WWS_Schedules CASCADE;
+CREATE CONSTRAINT TRIGGER check_total_work_hour_trigger
+	AFTER UPDATE startTime, endTime OR INSERT
+	ON WWS_Schedules
+	DEFERRABLE INITIALLY DEFERRED
+	FOR EACH ROW
+	EXECUTE FUNCTION check_total_work_hour();
+
+/*ensure there is a break between two slots*/
+CREATE OR REPLACE FUNCTION check_break() RETURNS TRIGGER AS $$
+DECLARE
+	startTime		SMALLINT;
+	endTime			SMALLINT;
+BEGIN
+	SELECT W.startTime, W.endTime INTO 
+
+
+/*ensure every hour interval has at least 5 riders*/
+CREATE OR REPLACE FUNCTION check_num_of_riders RETURNS TRIGGER AS $$
+
+
